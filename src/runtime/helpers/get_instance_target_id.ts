a6e9e114c81c5c3aa44c0b46c17c19af02a66b35
mjphooper <mjphooper@gmail.com>
2025-08-06 23:18:40 +0100

    fix: allow instance target names using dots
    During deployment, FUNCTION_TARGET or K_SERVICE was being supplied with the function name separated by dots and not dashes as expected. This was breaking module resolution.

--- src/runtime/helpers/get_instance_target_id.ts
+++ src/runtime/helpers/get_instance_target_id.ts
@@ -1,6 +1,6 @@
 
 export function getUnexpectedTargetNameErrorMessage(name: string) {
-  return `Unexpected function target name: "${name}". Expected pattern: one or more alphanumeric groups separated by dashes.`;
+  return `Unexpected function target name: "${name}". Expected pattern: one or more alphanumeric groups separated by dots (.) or dashes (-).`;
 }
 
 /**
@@ -10,13 +10,10 @@ export function getUnexpectedTargetNameErrorMessage(name: string) {
  * It checks the `FUNCTION_TARGET` environment variable first, and if it's not set,
  * falls back to `K_SERVICE`. The latter returns the function name in lowercase, as is
  * the {@link https://github.com/firebase/firebase-functions/issues/1279#issuecomment-1297447202|documented behavior of Cloud Run}.
- *  This fallback is necessary because Cloud Run may provide
- * the function name through `K_SERVICE`, often in lowercase and with hyphens.
- * Hyphens are replaced with dots, and the result is lowercased to ensure consistency.
  *
  * @param processEnv - The environment object, typically `process.env`, from which
  * target function identifiers are resolved.
- * @returns The resolved target ID in dot notation, or `null` if no ID could be found.
+ * @returns The resolved target ID in lowercase dot notation, or `null` if no ID could be found.
  *
  * See the list of reserved environment variables: https://firebase.google.com/docs/functions/config-env?gen=1st#reserved-names
  */
@@ -25,12 +22,14 @@ export function getInstanceTargetId(processEnv: NodeJS.ProcessEnv): string | nul
 
   if (!target) return null;
 
-  const isValidPattern = /^[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*$/.test(target);
+  const targetId = target.replace('-', '.').toLowerCase();
+
+  const isValidPattern = /^[a-z0-9]+(\.[a-z0-9]+)*$/.test(targetId);
   if (!isValidPattern) {
     throw new Error(
       getUnexpectedTargetNameErrorMessage(target),
     );
   }
 
-  return target.replaceAll('-', '.').toLowerCase();
+  return targetId;
 }
