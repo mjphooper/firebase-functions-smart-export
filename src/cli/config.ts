8c979b787fb2bccf2aa47e0354ac2be2960888fc
mjphooper <mjphooper@gmail.com>
2026-04-28 20:51:31 +0100

    refactor: merge define_config into config

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,4 +1,4 @@
-
+/* istanbul ignore file */
 
 /**
  * Configuration options for resolving and exporting Firebase Functions.
@@ -94,4 +94,24 @@ export interface Config {
    * Defaults to `false`.
    */
   useSingleQuotes?: boolean,
+}
+
+/**
+ * Defines a Firebase Functions Smart Export configuration object.
+ *
+ * This helper is used in your `ffse.config.ts` file to provide type safety
+ * and IntelliSense support when authoring configuration.
+ *
+ * The returned object should be the file's default export and not modified in
+ * any way.
+ *
+ * @example
+ * // ffse.config.ts
+ * export default defineConfig({
+ *   extensionIdentifier: "function",
+ *   useSingleQuotes: true,
+ * });
+ */
+export function defineConfig(config: Config): Config {
+  return config;
 }
